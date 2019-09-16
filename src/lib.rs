bc1653de98f200b4a6290df51b4c73d2be0b0b5a
Damien Gether <damiengether@gmail.com>
2019-09-15 20:01:06 -0400

    move Face to its own file

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,12 @@
 mod color_facet;
+mod face;
 mod facet;
 mod rotation_direction;
 
 pub use crate::color_facet::ColorFacet;
+pub use crate::face::Face;
 pub use crate::facet::Facet;
 pub use crate::rotation_direction::RotationDirection;
-
-#[derive(PartialEq, Clone, Copy)]
-pub struct Face {
-    pub center: Facet,
-    pub left_top: Facet,
-    pub top: Facet,
-    pub right_top: Facet,
-    pub left: Facet,
-    pub right: Facet,
-    pub left_bottom: Facet,
-    pub bottom: Facet,
-    pub right_bottom: Facet,
-}
 
 #[derive(PartialEq, Clone, Copy)]
 pub struct RubiksCube {
@@ -29,24 +18,6 @@ pub struct RubiksCube {
     pub yellow: Face,
 }
 
-impl std::fmt::Debug for Face {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(
-            f,
-            "{:?}{:?}{:?}\n{:?}{:?}{:?}\n{:?}{:?}{:?}",
-            self.left_top,
-            self.top,
-            self.right_top,
-            self.left,
-            self.center,
-            self.right,
-            self.left_bottom,
-            self.bottom,
-            self.right_bottom
-        )
-    }
-}
-
 impl std::fmt::Debug for RubiksCube {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         write!(
@@ -54,57 +25,6 @@ impl std::fmt::Debug for RubiksCube {
             "{:?}\n\n{:?}\n\n{:?}\n\n{:?}\n\n{:?}\n\n{:?}",
             self.red, self.blue, self.green, self.white, self.orange, self.yellow
         )
-    }
-}
-
-impl Face {
-    fn new(color: ColorFacet) -> Face {
-        Face {
-            left_top: Facet::new(color, 1),
-            top: Facet::new(color, 2),
-            right_top: Facet::new(color, 3),
-            left: Facet::new(color, 4),
-            center: Facet::new(color, 5),
-            right: Facet::new(color, 6),
-            left_bottom: Facet::new(color, 7),
-            bottom: Facet::new(color, 8),
-            right_bottom: Facet::new(color, 9),
-        }
-    }
-
-    fn rotate(self, direction: RotationDirection) -> Face {
-        match direction {
-            RotationDirection::Clockwise => Face::rotate_clockwise(self),
-            RotationDirection::Anticlockwise => Face::rotate_anticlockwise(self),
-        }
-    }
-
-    fn rotate_clockwise(self) -> Face {
-        Face {
-            left_top: self.left_bottom,
-            top: self.left,
-            right_top: self.left_top,
-            left: self.bottom,
-            right: self.top,
-            left_bottom: self.right_bottom,
-            bottom: self.right,
-            right_bottom: self.right_top,
-            ..self
-        }
-    }
-
-    fn rotate_anticlockwise(self) -> Face {
-        Face {
-            left_top: self.right_top,
-            top: self.right,
-            right_top: self.right_bottom,
-            left: self.top,
-            right: self.bottom,
-            left_bottom: self.left_top,
-            bottom: self.left,
-            right_bottom: self.left_bottom,
-            ..self
-        }
     }
 }
 
